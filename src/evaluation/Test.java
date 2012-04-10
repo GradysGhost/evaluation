9efc7330b5636f8489880b85551f02192e087b51
Ryan Jung <gradysghost@gmail.com>
2012-04-10 08:39:18 -0500

    Updated test class and added extensive README.

--- src/evaluation/Test.java
+++ src/evaluation/Test.java
@@ -3,6 +3,7 @@ package evaluation;
 public class Test {
 	
 	public static void main(String[] args) {
+		System.out.println(Evaluator.infixToPostfix(args[0]));
 		System.out.println(Evaluator.evaluateInfix(args[0]));
 	}
 	
